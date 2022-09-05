898b4aa6fc05d2e7dee44aa6be8e1d4dfeb36f38
Rob Donnelly <rfdonnelly@gmail.com>
2022-09-05 12:55:26 -0700

    wip: basic proof of concept working (see README)

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,131 @@
 use camino::{Utf8Path, Utf8PathBuf};
 use nix::errno::Errno;
-use nix::fcntl::{OFlag};
+use nix::fcntl::OFlag;
 use nix::pty::{grantpt, posix_openpt, ptsname_r, unlockpt, PtyMaster};
+use nix::sys::termios::{tcgetattr, cfmakeraw, tcsetattr, SetArg, LocalFlags};
 use thiserror::Error;
+use tokio_serial::{SerialPort, SerialStream};
+use tokio::fs::File;
+use tokio::io::{ReadHalf, WriteHalf};
+use tracing::info;
 
-use std::os::unix;
+use std::ffi::CStr;
 use std::fs;
+use std::os::unix::{self, io::FromRawFd, io::IntoRawFd};
+use std::os::unix::io::AsRawFd;
+use std::os::unix::prelude::RawFd;
+use std::os::unix::fs::FileTypeExt;
 
 #[derive(Error, Debug)]
-pub enum VspRouterError {
+pub enum Error {
     #[error("could not create pty")]
-    PtyCreate { source: Errno },
+    Pty(#[source] Errno),
 
     #[error("could not create link to pty")]
-    Link { source: std::io::Error },
+    Link(#[source] std::io::Error),
+
+    #[error("serial error")]
+    Serial(#[source] tokio_serial::Error),
 }
 
 pub struct Pty {
     manager: PtyMaster,
     name: String,
+}
+
+pub struct Tty {
+    master: SerialStream,
+    slave: SerialStream,
+}
+
+pub struct PtyLink {
     link: Utf8PathBuf,
 }
 
-type LibError = VspRouterError;
-type LibResult<T> = Result<T, LibError>;
+pub type Result<T> = std::result::Result<T, Error>;
 
 impl Pty {
-    pub fn new<P: AsRef<Utf8Path>>(link: P) -> LibResult<Self> {
-        let manager = posix_openpt(OFlag::O_RDWR)
-            .map_err(|source| VspRouterError::PtyCreate { source })?;
+    pub fn new() -> Result<Self> {
+        let manager =
+            posix_openpt(OFlag::O_RDWR | OFlag::O_NOCTTY).map_err(|src| Error::Pty(src))?;
 
-        grantpt(&manager)
-            .map_err(|source| VspRouterError::PtyCreate { source })?;
-        unlockpt(&manager)
-            .map_err(|source| VspRouterError::PtyCreate { source })?;
+        grantpt(&manager).map_err(|src| Error::Pty(src))?;
+        unlockpt(&manager).map_err(|src| Error::Pty(src))?;
 
         let name = ptsname_r(&manager)
-            .map_err(|source| VspRouterError::PtyCreate { source })?;
-        unix::fs::symlink(&name, link.as_ref().as_std_path())
-            .map_err(|source| VspRouterError::Link { source })?;
+            .map_err(|src| Error::Pty(src))?;
+
+        let mut termios = tcgetattr(manager.as_raw_fd())
+            .map_err(|src| Error::Pty(src))?;
+        cfmakeraw(&mut termios);
+        tcsetattr(manager.as_raw_fd(), SetArg::TCSANOW, &termios)
+            .map_err(|src| Error::Pty(src))?;
 
         Ok(Self {
             manager,
             name,
-            link: link.as_ref().to_path_buf(),
         })
     }
 
-    pub fn name(&self) -> &str {
-        &self.name
+    pub fn link<P: AsRef<Utf8Path>>(&self, path: P) -> Result<PtyLink> {
+        let link = path.as_ref().to_path_buf();
+        unix::fs::symlink(&self.name, link.as_std_path())
+            .map_err(|src| Error::Link(src))?;
+
+        Ok(PtyLink {
+            link,
+        })
     }
 
-    pub fn link(&self) -> &Utf8Path {
-        &self.link.as_path()
+    pub async fn split(self) -> (ReadHalf<File>, WriteHalf<File>) {
+        let file = unsafe { File::from_raw_fd(self.manager.into_raw_fd()) };
+        info!(metadata = ?file.metadata().await.unwrap());
+        tokio::io::split(file)
     }
 }
 
-impl Drop for Pty {
+impl Tty {
+    pub fn new() -> Result<Self> {
+        let (master, slave) = SerialStream::pair()
+            .map_err(|src| Error::Serial(src))?;
+
+        Ok(Self {
+            master,
+            slave,
+        })
+    }
+
+    pub fn link<P: AsRef<Utf8Path>>(&self, path: P) -> Result<PtyLink> {
+        let link = path.as_ref().to_path_buf();
+        info!("symlinking {} -> {}", self.slave.name().unwrap(), link);
+        unix::fs::symlink(&self.slave.name().unwrap(), link.as_std_path())
+            .map_err(|src| Error::Link(src))?;
+
+        Ok(PtyLink {
+            link,
+        })
+    }
+
+    pub async fn split(self) -> (ReadHalf<SerialStream>, WriteHalf<SerialStream>) {
+        // WORKAROUND: Prevent dropping the slave.  If we drop the slave, it closes the master and
+        // reads on the master will fail.
+        // TODO: Save the slave until app terminate so we can properly drop and clean it up.
+        std::mem::forget(self.slave);
+        tokio::io::split(self.master)
+    }
+}
+
+impl PtyLink {
+    pub fn link(&self) -> &Utf8Path {
+        &self.link.as_path()
+    }
+
+    pub fn id(&self) -> &str {
+        &self.link.as_str()
+    }
+}
+
+impl Drop for PtyLink {
     fn drop(&mut self) {
         if let Err(_) = fs::remove_file(&self.link) {
             eprintln!("error: could not delete {}", self.link);
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use vsp_router::Pty;
+use vsp_router::{Pty, Tty};
 
 use anyhow::{anyhow, Context};
+use bytes::Buf;
+use bytes::BufMut;
+use bytes::BytesMut;
 use clap::Parser;
+use futures::stream::FuturesUnordered;
+use futures::StreamExt;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use tracing::info;
 
+use std::collections::HashMap;
 use std::str::FromStr;
 
 type AppError = anyhow::Error;
@@ -11,12 +19,20 @@ type AppResult<T> = anyhow::Result<T>;
 #[derive(Parser)]
 #[clap(author, version, about)]
 struct Args {
-    /// Create a virtual serial port
-    #[clap(long = "virtual")]
+    /// Create a virtual serial port node.  Can use multiple times to create multiple virtual
+    /// serial ports.
+    #[clap(long = "virtual", id = "VIRTUAL")]
     virtuals: Vec<String>,
 
-    #[clap(long = "route", value_parser)]
+    /// Create a route between a source node and a destination node in the form of
+    /// '<src-id>:<dst-id>'.  Can use multiple times to create multiple routes.
+    #[clap(long = "route", id = "ROUTE")]
     routes: Vec<Route>,
+
+    /// Create a node attached to a physical serial port.  Can use multiple times to attached
+    /// multiple physical serial ports.
+    #[clap(long = "physical", id = "PHYSICAL")]
+    physicals: Vec<String>,
 }
 
 #[derive(Clone)]
@@ -30,24 +46,92 @@ impl FromStr for Route {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (src, dst) = s.split_once(':').ok_or(anyhow!("invalid route '{}'", s))?;
-        Ok(Self { src: src.to_string(), dst: dst.to_string(), })
+        Ok(Self {
+            src: src.to_string(),
+            dst: dst.to_string(),
+        })
     }
 }
 
-fn main() -> AppResult<()> {
+#[tokio::main]
+async fn main() -> AppResult<()> {
+    tracing_subscriber::fmt::init();
+
     let args = Args::parse();
 
-    let ptys = args.virtuals
-        .iter()
-        .map(|path| Pty::new(path))
-        .collect::<Result<Vec<_>, _>>()?;
+    // let ptys: HashMap<_, _> = args
+    //     .virtuals
+    //     .iter()
+    //     .map(|path| Pty::new(path).map(|pty| (pty.id().to_string(), pty)))
+    //     .collect::<Result<_, _>>()?;
 
-    for pty in ptys {
-        println!("pty: {} -> {}", pty.link(), pty.name());
-    }
+    // for (id, pty) in ptys {
+    //     info!("created pty: {} -> {}", pty.link(), pty.name());
+    // }
 
     for route in args.routes {
-        println!("route: {} -> {}", route.src, route.dst);
+        info!("created route: {} -> {}", route.src, route.dst);
+    }
+
+    let pty0 = Tty::new()?;
+    let pty0_link = pty0.link("0")?;
+    let pty1 = Tty::new()?;
+    let pty1_link = pty1.link("1")?;
+    let pty2 = Tty::new()?;
+    let pty2_link = pty2.link("2")?;
+    let (mut pty0_rd, mut pty0_wr) = pty0.split().await;
+    let (mut pty1_rd, mut pty1_wr) = pty1.split().await;
+    let (mut pty2_rd, mut pty2_wr) = pty2.split().await;
+
+    let mut buf0 = BytesMut::with_capacity(1024);
+    let mut buf1 = BytesMut::with_capacity(1024);
+    let mut buf2 = BytesMut::with_capacity(1024);
+    loop{
+        let (dsts, mut buf) = tokio::select!{
+            result = pty0_rd.read_buf(&mut buf0) => result.map(|_| (vec![&mut pty2_wr], &mut buf0)),
+            result = pty1_rd.read_buf(&mut buf1) => result.map(|_| (vec![&mut pty2_wr], &mut buf1)),
+            result = pty2_rd.read_buf(&mut buf2) => result.map(|_| (vec![&mut pty0_wr, &mut pty1_wr], &mut buf2)),
+        }?;
+        info!(?buf, remaining = buf.remaining(), remaining_mut = buf.remaining_mut(), "read data");
+
+        for dst in dsts {
+            let mut buf_clone = buf.clone();
+            dst.write_all_buf(&mut buf_clone).await?;
+            info!(?buf, remaining = buf.remaining(), remaining_mut = buf.remaining_mut(), "wrote data");
+        }
+        buf.clear();
+    }
+
+    // let mut buf0 = [0; 1024];
+    // let mut buf1 = [0; 1024];
+    // let mut buf2 = [0; 1024];
+    // loop{
+    //     let (dsts, mut buf, nbytes) = tokio::select!{
+    //         result = pty0_rd.read(&mut buf0) => result.map(|nbytes| (vec![&mut pty2_wr], &mut buf0, nbytes)),
+    //         result = pty1_rd.read(&mut buf1) => result.map(|nbytes| (vec![&mut pty2_wr], &mut buf1, nbytes)),
+    //         result = pty2_rd.read(&mut buf2) => result.map(|nbytes| (vec![&mut pty0_wr, &mut pty1_wr], &mut buf2, nbytes)),
+    //     }?;
+    //     info!(buf = ?buf[0..nbytes], "read data");
+
+    //     for dst in dsts {
+    //         dst.write_all(&mut buf[0..nbytes]).await?;
+    //         info!(buf = ?buf[0..nbytes], "wrote data");
+    //     }
+    // }
+}
+
+#[tracing::instrument(skip(src, dst))]
+async fn transfer_buf(
+    src_id: &str,
+    dst_id: &str,
+    src: &mut (impl AsyncRead + Unpin),
+    dst: &mut (impl AsyncWrite + Unpin),
+) -> AppResult<()> {
+    let mut buf = BytesMut::with_capacity(1024);
+    while let Ok(nbytes) = src.read_buf(&mut buf).await {
+        info!(?buf, remaining = buf.remaining(), remaining_mut = buf.remaining_mut(), "read data");
+        dst.write_buf(&mut buf).await?;
+        info!(?buf, remaining = buf.remaining(), remaining_mut = buf.remaining_mut(), "wrote data");
     }
 
     Ok(())
