28551437ebdaf0a0c1ae597e8e68e34d3517450a
Rob Donnelly <rfdonnelly@gmail.com>
2022-09-05 15:07:22 -0700

    feat: make ports and routes dynamic using streams and StreamMap

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use camino::{Utf8Path, Utf8PathBuf};
 use clap::Parser;
 use futures::stream::FuturesUnordered;
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use tokio_stream::StreamMap;
+use tokio_stream::StreamExt;
+use tokio_util::io::ReaderStream;
 use tracing::info;
 
 use std::collections::HashMap;
@@ -35,7 +38,7 @@ struct Args {
     physicals: Vec<String>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct IdPath {
     id: String,
     path: Utf8PathBuf,
@@ -63,7 +66,7 @@ impl FromStr for IdPath {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Route {
     src: String,
     dst: String,
@@ -87,105 +90,43 @@ async fn main() -> AppResult<()> {
 
     let args = Args::parse();
 
-    let mut sources = HashMap::new();
+    let mut sources = StreamMap::new();
     let mut sinks = HashMap::new();
     let mut links = Vec::new();
     for id_path in args.virtuals {
         let pty = Pty::new()?;
         let link = pty.link(&id_path.path)?;
         let (reader, writer) = pty.split();
-        sources.insert(id_path.id.clone(), reader);
+        sources.insert(id_path.id.clone(), ReaderStream::new(reader));
         sinks.insert(id_path.id.clone(), writer);
         links.push(link);
     }
 
+    // TODO: Don't include non-routed sources
+    // TODO: Warn on non-routed sources
+    // TODO: Validate IDs in routes
+
     let mut routes: HashMap<String, Vec<String>> = HashMap::new();
     for route in args.routes {
         routes.entry(route.src)
-            .and_modify(|dsts| dsts.push(route.dst))
-            .or_insert(Vec::new());
+            .or_insert(Vec::new())
+            .push(route.dst);
     }
+    info!(?routes);
 
-    let pty0 = Pty::new()?;
-    let pty0_link = pty0.link("0")?;
-    let pty1 = Pty::new()?;
-    let pty1_link = pty1.link("1")?;
-    let pty2 = Pty::new()?;
-    let pty2_link = pty2.link("2")?;
-    let (mut pty0_rd, mut pty0_wr) = pty0.split();
-    let (mut pty1_rd, mut pty1_wr) = pty1.split();
-    let (mut pty2_rd, mut pty2_wr) = pty2.split();
-
-    let mut buf0 = BytesMut::with_capacity(1024);
-    let mut buf1 = BytesMut::with_capacity(1024);
-    let mut buf2 = BytesMut::with_capacity(1024);
-    loop {
-        let (dsts, buf) = tokio::select! {
-            result = pty0_rd.read_buf(&mut buf0) => result.map(|_| (vec![&mut pty2_wr], &mut buf0)),
-            result = pty1_rd.read_buf(&mut buf1) => result.map(|_| (vec![&mut pty2_wr], &mut buf1)),
-            result = pty2_rd.read_buf(&mut buf2) => result.map(|_| (vec![&mut pty0_wr, &mut pty1_wr], &mut buf2)),
-        }?;
-        info!(
-            ?buf,
-            remaining = buf.remaining(),
-            remaining_mut = buf.remaining_mut(),
-            "read data"
-        );
-
-        for dst in dsts {
-            let mut buf_clone = buf.clone();
-            dst.write_all_buf(&mut buf_clone).await?;
-            info!(
-                ?buf,
-                remaining = buf.remaining(),
-                remaining_mut = buf.remaining_mut(),
-                "wrote data"
-            );
+    while let Some((src_id, result)) = sources.next().await {
+        // TODO: Unwrap will be OK when non-routed sources are filtered
+        let dst_ids = routes.get(&src_id).unwrap();
+        let bytes = result?;
+        info!(?src_id, ?dst_ids, ?bytes, "read");
+        for dst_id in dst_ids {
+            // TODO: Unwrap will be OK when IDs in routes are validated
+            let dst = sinks.get_mut(dst_id).unwrap();
+            let mut buf = bytes.clone();
+            dst.write_all_buf(&mut buf).await?;
+            info!(?dst_id, ?bytes, "wrote");
         }
-        buf.clear();
     }
 
-    // let mut buf0 = [0; 1024];
-    // let mut buf1 = [0; 1024];
-    // let mut buf2 = [0; 1024];
-    // loop{
-    //     let (dsts, mut buf, nbytes) = tokio::select!{
-    //         result = pty0_rd.read(&mut buf0) => result.map(|nbytes| (vec![&mut pty2_wr], &mut buf0, nbytes)),
-    //         result = pty1_rd.read(&mut buf1) => result.map(|nbytes| (vec![&mut pty2_wr], &mut buf1, nbytes)),
-    //         result = pty2_rd.read(&mut buf2) => result.map(|nbytes| (vec![&mut pty0_wr, &mut pty1_wr], &mut buf2, nbytes)),
-    //     }?;
-    //     info!(buf = ?buf[0..nbytes], "read data");
-
-    //     for dst in dsts {
-    //         dst.write_all(&mut buf[0..nbytes]).await?;
-    //         info!(buf = ?buf[0..nbytes], "wrote data");
-    //     }
-    // }
-}
-
-#[tracing::instrument(skip(src, dst))]
-async fn transfer_buf(
-    src_id: &str,
-    dst_id: &str,
-    src: &mut (impl AsyncRead + Unpin),
-    dst: &mut (impl AsyncWrite + Unpin),
-) -> AppResult<()> {
-    let mut buf = BytesMut::with_capacity(1024);
-    while let Ok(_nbytes) = src.read_buf(&mut buf).await {
-        info!(
-            ?buf,
-            remaining = buf.remaining(),
-            remaining_mut = buf.remaining_mut(),
-            "read data"
-        );
-        dst.write_buf(&mut buf).await?;
-        info!(
-            ?buf,
-            remaining = buf.remaining(),
-            remaining_mut = buf.remaining_mut(),
-            "wrote data"
-        );
-    }
-
-    Ok(())
+    return Ok(());
 }
