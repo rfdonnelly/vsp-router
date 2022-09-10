996ae5111f72eef68e1ee892869740c2450eb24f
Rob Donnelly <rfdonnelly@gmail.com>
2022-09-09 20:01:04 -0700

    refactor: move cli code to own module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use vsp_router::create_virtual_serial_port;
 
+mod cli;
+use crate::cli::Args;
+
 use anyhow::anyhow;
-use camino::Utf8PathBuf;
 use clap::Parser;
 use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
 use tokio_serial::SerialPortBuilderExt;
@@ -11,168 +13,9 @@ use tokio_util::sync::CancellationToken;
 use tracing::info;
 
 use std::collections::HashMap;
-use std::str::FromStr;
 
 type AppError = anyhow::Error;
 type AppResult<T> = anyhow::Result<T>;
-
-const CLAP_AFTER_HELP: &str = "
-EXAMPLES:
-
-    Share a physical serial port with two virtual serial ports.
-
-    Data sent from virtual serial port 0 is sent to the physical serial port but not to virtual
-    serial port 1.  Similarly, data sent from virtual serial port 1 is sent to the physical serial
-    port but not to virtual serial port 0.  Data received fromt the physical serial port is sent to
-    both virtual serial ports.
-
-    vsp-router \\
-        --virtual 0 \\
-        --virtual 1 \\
-        --physical 2:/dev/ttyUSB0,115200 \\
-        --route 0:2 \\
-        --route 1:2 \\
-        --route 2:0 \\
-        --route 2:1
-";
-
-#[derive(Parser)]
-#[clap(author, version, about, after_help = CLAP_AFTER_HELP)]
-struct Args {
-    /// Create a virtual serial port.
-    ///
-    /// The argument takes the following form: '[<id>:]<path>'
-    ///
-    /// If no ID is specified, the ID is set to the basename of the path.
-    ///
-    /// Can use multiple times to create multiple virtual serial ports.
-    ///
-    /// Examples:
-    ///
-    /// --virtual path/to/file
-    ///
-    ///     The path is 'path/to/file' and the ID is 'file'.
-    ///
-    /// --virtual 0:dev/ttyUSB0
-    ///
-    ///     The path is '/dev/ttyUSB0' and the ID is '0'.
-    #[clap(long = "virtual", id = "VIRTUAL", verbatim_doc_comment)]
-    virtuals: Vec<Virtual>,
-
-    /// Create a route between a source port and a destination port.
-    ///
-    /// The argument takes the following form: '<src-id>:<dst-id>'
-    ///
-    /// Can use multiple times to create multiple routes.
-    ///
-    /// Examples:
-    ///
-    /// --virtual 0:1
-    ///
-    ///     The source ID is '0' and the destination ID is '1'.
-    #[clap(long = "route", id = "ROUTE", verbatim_doc_comment)]
-    routes: Vec<Route>,
-
-    /// Open a physical serial port.
-    ///
-    /// The argument takes the following form: '[<id>:]<path>[,<baud-rate>]'
-    ///
-    /// If ID is not specified, the ID is set to the basename of the path. If baud rate is not
-    /// specificed, the baud rate defaults to 9600.
-    ///
-    /// Can use multiple times to attached multiple physical serial ports.
-    ///
-    /// Examples:
-    ///
-    /// --physical /dev/ttyUSB0
-    ///
-    ///     The path is '/dev/ttyUSB0', the ID is 'ttyUSB0', and the baud rate is 9600.
-    ///
-    /// --physical 1:/dev/ttyUSB0
-    ///
-    ///     The path is '/dev/ttyUSB0', the ID is '1', and the baud rate is 9600.
-    ///
-    /// --physical 1:/dev/ttyUSB0,115200
-    ///
-    ///     The path is '/dev/ttyUSB0', the ID is '1', and the baud rate is 115200.
-    #[clap(long = "physical", id = "PHYSICAL", verbatim_doc_comment)]
-    physicals: Vec<Physical>,
-}
-
-#[derive(Clone, Debug)]
-struct Virtual {
-    id: String,
-    path: Utf8PathBuf,
-}
-
-impl FromStr for Virtual {
-    type Err = AppError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.split_once(':') {
-            None => {
-                let path = Utf8PathBuf::from(s);
-                let id = path
-                    .file_name()
-                    .ok_or(anyhow!("invalid path '{s}'"))?
-                    .to_owned();
-                Ok(Self { id, path })
-            }
-            Some((id, path)) => {
-                let id = id.to_owned();
-                let path = Utf8PathBuf::from(path);
-                Ok(Self { id, path })
-            }
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-struct Route {
-    src: String,
-    dst: String,
-}
-
-impl FromStr for Route {
-    type Err = AppError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (src, dst) = s.split_once(':').ok_or(anyhow!("invalid route '{s}'"))?;
-        Ok(Self {
-            src: src.to_string(),
-            dst: dst.to_string(),
-        })
-    }
-}
-
-#[derive(Clone, Debug)]
-struct Physical {
-    id: String,
-    path: Utf8PathBuf,
-    baud_rate: u32,
-}
-
-impl FromStr for Physical {
-    type Err = AppError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (remainder, baud_rate) = match s.split_once(',') {
-            None => (s, 9600),
-            Some((remainder, baud_rate)) => {
-                let baud_rate = baud_rate.parse()?;
-                (remainder, baud_rate)
-            }
-        };
-
-        let id_path = Virtual::from_str(remainder)?;
-
-        Ok(Physical {
-            id: id_path.id,
-            path: id_path.path,
-            baud_rate,
-        })
-    }
-}
 
 #[tokio::main]
 async fn main() -> AppResult<()> {
